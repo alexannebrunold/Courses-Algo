ce34ce3683b3ef1d29393b5a7cb320fda0af3d48
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2019-12-12 11:27:09 +0100

    Exercice 3

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ while (b <= 10) {
 
 /* Exercice 3 Ecrire une fonction qui prend un paramètre qui affiche error si le chiffre et négatif ou sa table de multiplication si il est positif*/
 
-let a = 10;
+/*let a = 10;
 let i = 0;
 
 for (i = 0; i <= 10; i++) {
@@ -30,5 +30,16 @@ for (i = 0; i <= 10; i++) {
   else {
     console.log(i * a);
   }
-}
+}*/
 
+/* Exercice 4 Créer une fonction prenant 2 paramètres et retournant le plus grand des deux */
+
+let a = 2;
+let b = 3;
+
+if (b < a) {
+  console.log(a);
+}
+else {
+  console.log(b);
+}
