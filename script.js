b54c4da6e0eb5c3a6269023721d55dbf517d26a9
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2019-12-12 11:11:37 +0100

    EXercices 1 et 2

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Exercice 1 Ecrire une fonction qui affiche la table de multiplication de 5 */
+
+/*let a = 5;
+let b = 0;
+
+while (b <= 10) {
+  console.log(b * a);
+  b++;
+}*/
+
+/* Exercice 2 Ecrire une fonction qui prend un paramètre et qui affiche la table de multiplication de ce paramètre */
+
+let a = 234;
+let b = 1;
+
+while (b <= 10) {
+  console.log(b * a);
+  b++;
+}
